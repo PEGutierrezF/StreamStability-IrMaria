b75e11c04e9b6c9cbe6289867f92a7676cd1f736
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-05-08 15:46:01 -0600

    Graph Leaf Litter

--- 01 LeafLitter.R
+++ 01 LeafLitter.R
@@ -44,7 +44,11 @@ p1 <- ggplot(LeafLitter,aes(TimeLeaf ,
   theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
   theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis y
 
-  ylim(-3,0)
+  ylim(-3,0) +
+  
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
 p1
 
   # Lm QPB Leaf Litter ------------------------------------------------------
@@ -63,14 +67,17 @@ p2 <- ggplot(LeafLitter,aes(TimeLeaf ,
   geom_point(size = 3) + 
   geom_smooth(method=lm,se=FALSE) +
   
-  xlab('15-days sampling')+ ylab("Mean litter input rate ("*~g~m^-2~d^-1*")") +
+  xlab('Sampling period')+ ylab("Mean litter input rate ("*~g~m^-2~d^-1*")") +
   theme(axis.title.x = element_text(size = 18, angle = 00)) + # axis x
   theme(axis.title.y = element_text(size = 18, angle = 90)) +
   
   theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
   theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis y
 
-ylim(-3,0)
+ylim(-3,0) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
 p2
 
 leaf <- p1 / p2
@@ -82,9 +89,13 @@ leaf + ggsave("Leaf.jpeg", width=6, height=10,dpi=600)
 
 # QPA
 p3 <- ggplot(variables,aes(TimeLeaf ,
-                            y=QPAMean))+
+                            y=QPALeaf))+
   geom_point() + 
   geom_line() +
+  geom_errorbar(aes(ymin=QPALeaf-QPAsdLeaf   , ymax=QPALeaf+QPAsdLeaf), width=.2,
+                position=position_dodge(0.05)) + 
+  geom_segment(aes(x = 1, y = 1.33, xend = 16, yend = 1.33))+
+  geom_segment((aes(x = 17, y = 1.33, xend = 41, yend = 1.33)), color="red", linetype="dashed", size=1) + 
 
   xlab('')+ ylab("Mean litter input rate ("*~g~m^-2~d^-1*")") +
   theme(axis.title.y = element_text(size = 18, angle = 90)) +
@@ -92,24 +103,38 @@ p3 <- ggplot(variables,aes(TimeLeaf ,
   theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
   theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis y
   
-  ylim(0,10)
+  ylim(0,10) +
+  
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+
 p3
 
 # QPB
 
 p4 <- ggplot(variables,aes(TimeLeaf ,
-                           y=QPBMean))+
+                           y=QPBLeaf))+
   geom_point() + 
   geom_line() +
-  
-  xlab('15-days sampling')+ ylab("Mean litter input rate ("*~g~m^-2~d^-1*")") +
+  geom_errorbar(aes(ymin=QPBLeaf-QPBsdLeaf   , ymax=QPBLeaf+QPBsdLeaf), width=.2,
+                position=position_dodge(0.05)) + 
+  geom_segment(aes(x = 1, y = 1.40, xend = 16, yend = 1.40))+
+  geom_segment((aes(x = 17, y = 1.40, xend = 41, yend = 1.40)), color="red", linetype="dashed", size=1) + 
+
+  xlab('Sampling period')+ ylab("Mean litter input rate ("*~g~m^-2~d^-1*")") +
   theme(axis.title.x = element_text(size = 18, angle = 00)) + # axis x
   theme(axis.title.y = element_text(size = 18, angle = 90)) +
   
   theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
   theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis y
   
-  ylim(0,10)
+  ylim(0,10) +
+  
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+
 p4
 
 
