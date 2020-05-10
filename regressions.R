d6db4fc46dcf4ba390f06ef61122874343b35252
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-05-09 20:36:50 -0600

    regressions

--- regressions.R
+++ regressions.R
@@ -22,15 +22,19 @@ ggplot(regression, aes(Time,
                        value, 
                        shape= variable , 
                        col = stream)) + 
-  geom_point(size = 5) +
-  geom_smooth(method = 'lm', se = F) +
+ geom_point(size = 5) +
+ geom_smooth(method = 'lm', se = T,  aes(color=stream)) + #, alpha = .15, aes(fill = stream)) +
 
-xlab('Sampling period') + ylab("Vaarible ("*"\u03BC"~g~m^-2*")") + 
+
+xlab('Sampling period') + ylab("Vaarible (random units)") + 
   theme(axis.title.x = element_text(size = 16, angle = 0)) +# axis x
   theme(axis.title.y = element_text(size = 16, angle = 90)) +
+  
+  geom_hline(yintercept = 0, color="darkred") +
 
 theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
       panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
-
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5)) +
+  # facet_grid(. ~ variable)
+  # facet_wrap(. ~ variable)
 
