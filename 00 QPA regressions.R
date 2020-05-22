ec0ff9e114ad8a86612c77ebeb9832fa3c1f8ffc
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-05-21 21:55:25 -0600

    Data QPA

--- 00 QPA regressions.R
+++ 00 QPA regressions.R
@@ -15,13 +15,13 @@ library(ggplot2)
 library(dplyr)
 library(patchwork)
 
-QPAregression<- read.csv("00 QPAregressions.csv")
+QPAregression<- read.csv("00 QPA regressions.csv")
 QPAregression
 
 QPAregression$variable_f = factor(QPAregression$variable, levels=c('Canopy','Leaf_litter','Shrimp',
                                                                    'Chlorophyll-a','Macroinvertebrates'))
 
-Regression <- ggplot(QPAregression, aes(Time, 
+RegressionQPA <- ggplot(QPAregression, aes(Time, 
                                      value,
                                      color=variable)) + 
   geom_smooth(se = F, size=5)  + 
@@ -36,18 +36,19 @@ xlab('Sampling period') + ylab("Change in magnitude") +
 #  annotate("text", x = 5, y = 2.5, label = "Increase",colour = "black", size = 6,angle = 90) +
 #  annotate("text", x = 5, y = -2.5, label = "Decrease",colour = "black", size = 6,angle = 90) +
   
- # theme(legend.title=element_blank())+
- # theme(legend.text = element_text(size=20)) +
+  #theme(legend.title=element_blank())+
+  #theme(legend.text = element_text(size=20)) +
   theme(legend.position="none")+
   
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5)) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5)) + 
   
   theme(strip.text.x  = element_text(size = 14, colour = "black", angle = 0)) +
   theme(strip.background = element_rect(colour = "black", fill = "grey94")) +
   
   facet_grid(~ variable_f, scales="free_x")
 
-Regression
-  
+RegressionQPA
+
+RegressionQPA + ggsave("ResistanceQPA.jpeg", width=10, height=6,dpi=600)  
