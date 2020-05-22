71d8b8afaeb87c30c648ee890cde4e8f06771b38
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-05-21 20:18:31 -0600

    Data QPA

--- 00 QPA regressions.R
+++ 00 QPA regressions.R
@@ -18,10 +18,12 @@ library(patchwork)
 QPAregression<- read.csv("00 QPAregressions.csv")
 QPAregression
 
+QPAregression$variable_f = factor(QPAregression$variable, levels=c('Canopy','Leaf_litter','Shrimp','Chlorophyll-a'))
+
 Regression <- ggplot(QPAregression, aes(Time, 
                                      value,
                                      color=variable)) + 
-  geom_smooth(se = F)  + 
+  geom_smooth(se = F, size=5)  + 
 
   geom_hline(yintercept = 0, color="darkred") +
 
@@ -30,8 +32,8 @@ xlab('Sampling period') + ylab("Changes in magnitude") +
   theme(axis.title.y = element_text(size = 16, angle = 90)) +
   
   ylim(-3,3) + 
-  annotate("text", x = 5, y = 2.5, label = "Increase",colour = "black", size = 6,angle = 90) +
-  annotate("text", x = 5, y = -2.5, label = "Decrease",colour = "black", size = 6,angle = 90) +
+#  annotate("text", x = 5, y = 2.5, label = "Increase",colour = "black", size = 6,angle = 90) +
+#  annotate("text", x = 5, y = -2.5, label = "Decrease",colour = "black", size = 6,angle = 90) +
   
  # theme(legend.title=element_blank())+
  # theme(legend.text = element_text(size=20)) +
@@ -44,7 +46,7 @@ xlab('Sampling period') + ylab("Changes in magnitude") +
   theme(strip.text.x  = element_text(size = 14, colour = "black", angle = 0)) +
   theme(strip.background = element_rect(colour = "black", fill = "grey94")) +
   
-   facet_grid(~ variable, scales="free_x")
+  facet_grid(~ variable_f, scales="free_x")
 
 Regression
   
