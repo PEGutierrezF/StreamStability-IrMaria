1cdbb57693dcb31e09c5f948da76519d5de0e27f
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-05-21 19:35:15 -0600

    Graph

---
+++ 00 QPA regressions.R
@@ -0,0 +1,50 @@
+
+
+
+
+#--------------------------------------------
+# QPA regression
+# 21 May 2020
+#PEGF
+#--------------------------------------------
+#
+
+
+library(modelr)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+
+QPAregression<- read.csv("00 QPAregressions.csv")
+QPAregression
+
+Regression <- ggplot(QPAregression, aes(Time, 
+                                     value,
+                                     color=variable)) + 
+  geom_smooth(se = F)  + 
+
+  geom_hline(yintercept = 0, color="darkred") +
+
+xlab('Sampling period') + ylab("Changes in magnitude") + 
+  theme(axis.title.x = element_text(size = 16, angle = 0)) +# axis x
+  theme(axis.title.y = element_text(size = 16, angle = 90)) +
+  
+  ylim(-3,3) + 
+  annotate("text", x = 5, y = 2.5, label = "Increase",colour = "black", size = 6,angle = 90) +
+  annotate("text", x = 5, y = -2.5, label = "Decrease",colour = "black", size = 6,angle = 90) +
+  
+ # theme(legend.title=element_blank())+
+ # theme(legend.text = element_text(size=20)) +
+  theme(legend.position="none")+
+  
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5)) +
+  
+  theme(strip.text.x  = element_text(size = 14, colour = "black", angle = 0)) +
+  theme(strip.background = element_rect(colour = "black", fill = "grey94")) +
+  
+   facet_grid(~ variable, scales="free_x")
+
+Regression
+  
