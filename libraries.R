e80797b243542de2cb9227973fa6efb7930815be
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-02-01 19:38:57 -0600

    library

---
+++ libraries.R
@@ -0,0 +1,35 @@
+
+
+
+
+
+#--------------------------------------------
+# Idealizaed trajectories
+# 29 May 2020
+#PEGF
+#--------------------------------------------
+#
+
+install.packages("plyr")
+libraries <- c("modelr", "ggplot2", "plyr","dplyr", 'patchwork')
+lapply(libraries, require, character.only = TRUE)
+
+
+
+library(modelr)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+
+
+ideal<- read.csv("00 ideal.csv")
+ideal
+
+
+Ideal <- ggplot(ideal, aes(Time, 
+                              Value ,
+                              color=Variable)) + 
+   geom_smooth(se = F, size=5) 
+
+Ideal
+  
