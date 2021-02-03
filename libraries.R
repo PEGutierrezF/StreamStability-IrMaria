0f7be499a76afe58a31ab89587da59ab69849b65
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-02-02 20:30:08 -0600

    data

--- libraries.R
+++ libraries.R
@@ -15,22 +15,3 @@ libraries <- c("modelr", "ggplot2", "plyr","dplyr", 'patchwork',
                "reshape2", "ggpubr")
 lapply(libraries, require, character.only = TRUE)
 
-
-
-library(modelr)
-library(ggplot2)
-library(dplyr)
-library(patchwork)
-
-
-ideal<- read.csv("00 ideal.csv")
-ideal
-
-
-Ideal <- ggplot(ideal, aes(Time, 
-                              Value ,
-                              color=Variable)) + 
-   geom_smooth(se = F, size=5) 
-
-Ideal
-  
