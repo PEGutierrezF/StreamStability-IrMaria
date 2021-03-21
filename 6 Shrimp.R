f79309660bd1b860200ea64622a27eba245a84c0
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-03-21 16:21:33 -0600

    data

--- 6 Shrimp.R
+++ 6 Shrimp.R
@@ -9,17 +9,17 @@
 #--------------------------------------------
 #
 
-Shrimp<- read.csv("6 Shrimp.csv")
+Shrimp<- read.csv("data/Shrimps.csv")
 Shrimp
 
 
 # Lm QPA Leaf Litter ------------------------------------------------------
 
-Shrimp <- Shrimp %>% select(TimeCHLA, QPAShrimpLog, QPBShrimpLog)
+Shrimp <- Shrimp %>% select(events, QPAShrimpLog, QPBShrimpLog)
 Shrimp <- na.omit(Shrimp)
 Shrimp
 
-QPAShrimp.mod  <- lm(QPAShrimpLog ~ TimeCHLA, na.action=na.omit, data=Shrimp)
+QPAShrimp.mod  <- lm(QPAShrimpLog ~ events, na.action=na.omit, data=Shrimp)
 summary(QPAShrimp.mod)
 
 Shrimp$QPAresid<- QPAShrimp.mod$resid
@@ -32,7 +32,7 @@ Shrimp
 # Shrimp QPB --------------------------------------------------------------
 
 
-QPBShrimp.mod  <- lm(QPBShrimpLog ~ TimeCHLA, na.action=na.omit, data=Shrimp)
+QPBShrimp.mod  <- lm(QPBShrimpLog ~ events, na.action=na.omit, data=Shrimp)
 summary(QPBShrimp.mod)
 
 Shrimp$QPBresid<- QPBShrimp.mod$resid
