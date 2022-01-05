93c625bdaf347962094600d2e7c6fcdbd4337094
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-01-05 13:30:59 -0600

    data

--- 6 Macroinvertebrates.R
+++ 6 Macroinvertebrates.R
@@ -20,8 +20,8 @@ rm(list=ls())
 macroinvertebrates <- read.csv("data/all_data.csv")
 head(macroinvertebrates)
 
-macroiv <- macroinvertebrates %>% dplyr::select(date_shrimp, QPA_shrimp, QPB_shrimp)
-macroiv$date_shrimp <- as.POSIXct(macroiv$date_shrimp,"%Y-%m-%d",tz = "UTC")
+macroiv <- macroinvertebrates %>% dplyr::select(date_macroiv, QPA_miv, QPB_miv)
+macroiv$date_macroiv <- as.POSIXct(macroiv$date_macroiv,"%Y-%m-%d",tz = "UTC")
 macroiv <- na.omit(macroiv)
 
 
@@ -29,33 +29,62 @@ macroiv <- na.omit(macroiv)
 # Linear model Macroinvertebrate abundance Prieta A ---------------------
 ################################################################
 
-# Lm QPA Macroinvertebrates ------------------------------------------------------
+QPA.macroinv.mod  <- lm(QPA_miv ~ date_macroiv, data=macroiv)
+summary(QPA.macroinv.mod)
 
-Macroinvertebrates <- Macroinvertebrates %>% select(TimeMacro, QPAMacroinvLog, QPBMacroinvLog)
-Macroinvertebrates <- na.omit(Macroinvertebrates)
-Macroinvertebrates
+macroiv$QPAresid<- QPA.macroinv.mod$resid
+head(macroiv)
 
-QPAMacroinv.mod  <- lm(QPAMacroinvLog ~ TimeMacro, data=Macroinvertebrates)
-summary(QPAMacroinv.mod)
-
-Macroinvertebrates$QPAresid<- QPAMacroinv.mod$resid
-Macroinvertebrates
-
-1/apply(Macroinvertebrates, 2, sd)
+1/apply(macroiv, 2, sd)
 
 
-
+qpa.miv <- ggplot(macroiv, aes(date_macroiv, y=QPA_miv))+
+  geom_point(size = 3) + 
+  geom_smooth(method=lm,se=FALSE) +
+  
+  xlab('') + ylab("Residuals") + # axis x
+  theme(axis.title.y = element_text(size = 18, angle = 90)) +
+  
+  theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis y
+  
+  ylim(-2,2) +
+  
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+qpa.miv
 
 # LM QPB Macroinvertebrates  ----------------------------------------------
 
 
-QPBMacroinv.mod  <- lm(QPBMacroinvLog ~ TimeMacro, data=Macroinvertebrates)
-summary(QPBMacroinv.mod)
+QPB.macroinv.mod  <- lm(QPB_miv ~ date_macroiv, data=macroiv)
+summary(QPB.macroinv.mod)
 
-Macroinvertebrates$QPBresid<- QPBMacroinv.mod$resid
-Macroinvertebrates
+macroiv$QPAresid<- QPB.macroinv.mod$resid
+head(macroiv)
 
-1/apply(Macroinvertebrates, 2, sd)
+1/apply(macroiv, 2, sd)
 
 
+qpb.miv <- ggplot(macroiv, aes(date_macroiv, y=QPB_miv))+
+  geom_point(size = 3) + 
+  geom_smooth(method=lm,se=FALSE) +
+  
+  xlab('') + ylab("Residuals") + # axis x
+  theme(axis.title.y = element_text(size = 18, angle = 90)) +
+  
+  theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis y
+  
+  ylim(-2,2) +
+  
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
+qpb.miv
+
+
+plot.miv <- qpa.miv / qpb.miv
+plot.miv + ggsave("Regression macroinvertebrate abundance.jpeg",  path = "figures", width=6, height=10,dpi=600)
 
