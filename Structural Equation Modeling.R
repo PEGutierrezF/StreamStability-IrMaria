bae777afa32e37ac366785f0ad7ed8bc8d7c9193
PEGutierrF <pgutierr@uvm.edu>
2024-02-28 13:44:59 -0500

    sem

--- Structural Equation Modeling.R
+++ Structural Equation Modeling.R
@@ -18,26 +18,24 @@ rm(list = ls())
 data_sem <- read.xlsx("data/data_SEM.xlsx", detectDates = TRUE)
 head(data_sem)
 
-library(mvnormalTest)
+
 df <- mardia(data_sem)
 df$uv.shapiro
 
-mod1 <- '
-decapod ~ epilithon
-decapod ~ canopy + epilithon
+model <- psem(
+  
+  glm(epilithon ~ macroinvertebrates + decapod + canopy, poisson(link = "log"),
+      data = data_sem, na.action = na.exclude),
+  
+  glm(decapod ~  epilithon + canopy, poisson(link = "log"),
+     data = data_sem, na.action = na.exclude),
 
-epilithon ~ canopy
-epilithon ~ decapod
+  glm(macroinvertebrates ~  epilithon + decapod + canopy, poisson(link = "log"),
+      data = data_sem, na.action = na.exclude)
 
-macroinvertebrates ~ decapod
-macroinvertebrates ~ epilithon 
-macroinvertebrates ~ canopy + epilithon + decapod'
-'
+  )
 
-fit1 <- sem(mod1, data=data_sem, std.lv = TRUE, estimator = "MLM")
-summary(fit1)
-
-fitMeasures(fit1, c("chisq.scaled", "df.scaled", "pvalue.scaled"))
+summary(model, .progressBar = F)
 
 
 References
