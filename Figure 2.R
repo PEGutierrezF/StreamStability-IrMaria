a2b3c6f75e0e48b64618f2151fda4b6b77d7ffee
PEGutierrF <pgutierr@uvm.edu>
2024-05-16 16:35:22 -0400

    data

--- Figure 2.R
+++ Figure 2.R
@@ -18,22 +18,26 @@ rm(list = ls())
 traj_QPA <- read.xlsx("data/data_trajectories.xlsx", sheet='var_environ',
                       detectDates = TRUE)
 
+traj_QPA <- traj_QPA %>% 
+  dplyr::select(1:11)
+
+
 data_long <- traj_QPA %>%
   pivot_longer(cols = starts_with("Temp") | starts_with("Cond") | starts_with("Potassium") | starts_with("DOC") | starts_with("Nitrate"),
-               names_to = c("Parameter", "Stream"),
+               names_to = c("variable", "stream"),
                names_pattern = "(.*)_(QPA|QPB)",
                values_to = "Value")
 
 
 
-variable_new <- c("Cond"= "textstyle('Conductivity')", 
+variable_new <- c("Cond"= "textstyle('Conductivity')~(mu*S/cm)", 
                   "DOC"="textstyle('DOC')",
                   "Nitrate"="textstyle('Nitrate')",  #Chlorophyll-')*italic('a')
                   "Potassium"="textstyle('Potassium')",
-                  "Temp"= "textstyle(Temperature)")
+                  "Temp"= "textstyle(Temperature)~(degree*C)")
 
 
-data_long$Parameter <- factor(data_long$Parameter,      # Reordering group factor levels
+data_long$variable <- factor(data_long$variable,      # Reordering group factor levels
                                 levels = c("Temp", "Cond", "DOC", "Nitrate", "Potassium"))
 
 streams_new <- c("QPA"="Prieta A", "QPB"="Prieta B")
@@ -41,7 +45,7 @@ streams_new <- c("QPA"="Prieta A", "QPB"="Prieta B")
 
 
 
-ggplot(data_long, aes(x = date_QPA, y = Value)) + 
+a <- ggplot(data_long, aes(x = date_QPA, y = Value)) + 
   geom_point(shape = 21, fill = "#bdd7e7", color = "#2171b5", size = 3) +
   geom_smooth(se = T, size=1.7, color= "gray20", method = "gam", formula = y ~s(x)) + 
   geom_hline(yintercept = 0, color="gray20") +
@@ -52,7 +56,7 @@ ggplot(data_long, aes(x = date_QPA, y = Value)) +
   theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
   theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis y
   
-  #ylim(-3,3) + 
+ # ylim(-3,3) + 
   
   theme(legend.position="none")+
   
@@ -67,17 +71,17 @@ ggplot(data_long, aes(x = date_QPA, y = Value)) +
   #geom_vline(aes(xintercept=as.POSIXct("2022-03-1")), # Stream FRE
   #        col= "red",linetype=4, alpha=0.9, size = 1) +
   
-  facet_grid(Stream ~ Parameter,
-             labeller = labeller(Parameter = as_labeller(variable_new, label_parsed),
-                                 Stream  = streams_new)) +
   theme(strip.text.x = element_text(size = 14, color = "black"),
         strip.text.y = element_text(size = 14, color = "black"),
         strip.placement = "outside") +
   theme(strip.background=element_rect(color= "black", fill="gray85")) +
   theme(strip.text.x = element_text(margin = margin(0.001,0,0.001,0, "cm"))) +
-  theme(strip.switch.pad.grid = unit('0.5', "cm"))
-
-
+  theme(strip.switch.pad.grid = unit('0.5', "cm")) +
+  
+  facet_grid(stream ~ variable, 
+           labeller = labeller(variable = as_labeller(variable_new, label_parsed),
+                               stream  = streams_new)) 
+a
 
 
 
