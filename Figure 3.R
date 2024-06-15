5b16e9579de3abaa5b72443101e178d558f90992
PEGutierrF <pgutierr@uvm.edu>
2024-06-14 20:50:27 -0400

    data

--- Figure 3.R
+++ Figure 3.R
@@ -6,7 +6,7 @@ plot <- grViz("
 digraph boxes_and_circles{
 
   # a 'graph' statement with increased margin
-  graph [overlap = true]
+  graph [overlap = false]
 
   # first subgraph (diagram 1)
     subgraph cluster_1 {
@@ -24,13 +24,13 @@ digraph boxes_and_circles{
     # several 'edge' statements with numbers
     edge [minlen = 1, arrowhead=vee]
     'Canopy openness' -> 'Leaf litter' [label = '-0.40\n(-0.64; 0.16)', fontsize = 12]
-    'Canopy openness' -> 'Epilithic algae' [label = '-2.85\n(-6.94; -0.83)', fontsize = 12]
+    'Canopy openness' -> 'Epilithic algae' [label = '-2.85\n   (-6.94; -0.83)', fontsize = 12]
     'Leaf litter' -> 'Decapoda abundance' [label = 'N.E.', style = dotted, fontsize = 12]
-    'Epilithic algae' -> 'Decapoda abundance' [label = '-1.89\n(-3.59; -0.90)', fontsize = 12]
+    'Epilithic algae' -> 'Decapoda abundance' [label = '-1.89\n   (-3.59; -0.90)', fontsize = 12]
     'Decapoda abundance' -> 'Epilithic algae' [label = '4.73\n(0.91; 12.04)', fontsize = 12]
     'Leaf litter' -> 'Macroinvertebrate\ndensity' [label = 'N.E.', style = dotted, fontsize = 12]
     'Epilithic algae' -> 'Macroinvertebrate\ndensity' [label = 'N.E.', style = dotted, fontsize = 12]
-    'Decapoda abundance' -> 'Macroinvertebrate\ndensity' [label = 'N.E.', style = dotted, fontsize = 12]
+    'Decapoda abundance' -> 'Macroinvertebrate\ndensity' [label = '  N.E.', style = dotted, fontsize = 12]
 
     { rank = same; 'Leaf litter'; 'Epilithic algae'; }
 }  //for the Squared
@@ -51,13 +51,13 @@ digraph boxes_and_circles{
     # several 'edge' statements with numbers
     edge [minlen = 1, arrowhead=vee]
     'Canopy openness ' -> 'Leaf litter ' [label = 'N.E.', style = dotted, fontsize = 12]
-    'Canopy openness ' -> 'Epilithic algae ' [label = '-1.72\n(-4.61 ; -0.67)', fontsize = 12]
+    'Canopy openness ' -> 'Epilithic algae ' [label = '-1.72\n   (-4.61; -0.67)', fontsize = 12]
     'Leaf litter ' -> 'Decapoda abundance ' [label = '0.55\n(0.07;1.44)', fontsize = 12]
     'Epilithic algae ' -> 'Decapoda abundance ' [label = '-1.70\n(-3.72; -0.70)', fontsize = 12]
     'Decapoda abundance ' -> 'Epilithic algae ' [label = '2.66\n(0.39; 8.98)', fontsize = 12]
     'Leaf litter ' -> 'Macroinvertebrate\ndensity ' [label = 'N.E.', style = dotted, fontsize = 12]
     'Epilithic algae ' -> 'Macroinvertebrate\ndensity ' [label = 'N.E.', style = dotted, fontsize = 12]
-    'Decapoda abundance ' -> 'Macroinvertebrate\ndensity ' [label = 'N.E.', style = dotted, fontsize = 12]
+    'Decapoda abundance ' -> 'Macroinvertebrate\ndensity ' [label = '  N.E.', style = dotted, fontsize = 12]
 
     { rank = same; 'Leaf litter '; 'Epilithic algae '; }
 
@@ -70,6 +70,6 @@ export_svg(plot) %>%
   charToRaw %>% 
   rsvg(width = 10000, 
        height = 6000) %>% 
-  tiff::writeTIFF("Figure 3.tiff")
+  tiff::writeTIFF("Figure 3.tiff", bits.per.sample = 8L)
 
 
