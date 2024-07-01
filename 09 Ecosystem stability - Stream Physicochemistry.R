9b896b962be8b231a218f504f76b3eb412fd7ff3
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2024-07-01 14:46:39 -0400

    data

--- 09 Ecosystem stability - Stream Physicochemistry.R
+++ 09 Ecosystem stability - Stream Physicochemistry.R
@@ -56,7 +56,7 @@ res.QPA.cond.mod <- residuals(QPA.cond.mod)
 1/sd(res.QPA.cond.mod)
 
 
-# Conductivity Prieta A ----------------------------------------------------
+# Conductivity Prieta B ----------------------------------------------------
 cond_QPB <- env_QPx %>%
   dplyr::select(date, cond_QPB)
 cond_QPB$date <- as.POSIXct(cond_QPB$date,"%Y-%m-%d",tz = "UTC")
@@ -68,3 +68,84 @@ summary(QPB.cond.mod)
 # Temporal stability
 res.QPB.cond.mod <- residuals(QPB.cond.mod)
 1/sd(res.QPB.cond.mod)
+
+
+# Potassium Prieta A ----------------------------------------------------
+k_QPA <- env_QPx %>%
+  dplyr::select(date, potassium_QPA)
+k_QPA$date <- as.POSIXct(k_QPA$date,"%Y-%m-%d",tz = "UTC")
+
+QPA.k.mod  <- lm(potassium_QPA ~ date, data=k_QPA)
+summary(QPA.k.mod)
+
+# Temporal stability
+res.QPA.k.mod <- residuals(QPA.k.mod)
+1/sd(res.QPA.k.mod)
+
+
+# Potassium Prieta B ----------------------------------------------------
+k_QPB <- env_QPx %>%
+  dplyr::select(date, potassium_QPB)
+k_QPB$date <- as.POSIXct(k_QPB$date,"%Y-%m-%d",tz = "UTC")
+head(k_QPB)
+
+QPB.k.mod  <- lm(potassium_QPB ~ date, data=k_QPB)
+summary(QPB.k.mod)
+
+# Temporal stability
+res.QPB.k.mod <- residuals(QPB.k.mod)
+1/sd(res.QPB.k.mod)
+
+
+# Nitrate Prieta A ----------------------------------------------------
+NO3_QPA <- env_QPx %>%
+  dplyr::select(date, nitrate_QPA)
+NO3_QPA$date <- as.POSIXct(NO3_QPA$date,"%Y-%m-%d",tz = "UTC")
+
+QPA.NO3.mod  <- lm(nitrate_QPA ~ date, data=NO3_QPA)
+summary(QPA.NO3.mod)
+
+# Temporal stability
+res.QPA.NO3.mod <- residuals(QPA.NO3.mod)
+1/sd(res.QPA.NO3.mod)
+
+
+# Nitrate Prieta B ----------------------------------------------------
+NO3_QPB <- env_QPx %>%
+  dplyr::select(date, nitrate_QPB)
+NO3_QPB$date <- as.POSIXct(NO3_QPB$date,"%Y-%m-%d",tz = "UTC")
+head(NO3_QPB)
+
+QPB.NO3.mod  <- lm(nitrate_QPB ~ date, data=NO3_QPB)
+summary(QPB.NO3.mod)
+
+# Temporal stability
+res.QPB.NO3.mod <- residuals(QPB.NO3.mod)
+1/sd(res.QPB.NO3.mod)
+
+
+#  Dissolved Organic Carbon Prieta A ----------------------------------------------------
+doc_QPA <- env_QPx %>%
+  dplyr::select(date, doc_QPA)
+doc_QPA$date <- as.POSIXct(doc_QPA$date,"%Y-%m-%d",tz = "UTC")
+
+QPA.doc.mod  <- lm(doc_QPA ~ date, data=doc_QPA)
+summary(QPA.doc.mod)
+
+# Temporal stability
+res.QPA.doc.mod <- residuals(QPA.doc.mod)
+1/sd(res.QPA.doc.mod)
+
+
+# Dissolved Organic Carbon Prieta B ----------------------------------------------------
+doc_QPB <- env_QPx %>%
+  dplyr::select(date, doc_QPB)
+doc_QPB$date <- as.POSIXct(doc_QPB$date,"%Y-%m-%d",tz = "UTC")
+head(doc_QPB)
+
+QPB.doc.mod  <- lm(doc_QPB ~ date, data=doc_QPB)
+summary(QPB.doc.mod)
+
+# Temporal stability
+res.QPB.doc.mod <- residuals(QPB.doc.mod)
+1/sd(res.QPB.doc.mod)
