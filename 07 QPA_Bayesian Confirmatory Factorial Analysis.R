b98f048b496e19ee3406099dc76948263a825447
PEGutierrezF <pabloe.gutierrezfonseca@gmail.com>
2025-05-30 22:06:01 -0400

    warnings

--- 07 QPA_Bayesian Confirmatory Factorial Analysis.R
+++ 07 QPA_Bayesian Confirmatory Factorial Analysis.R
@@ -30,29 +30,23 @@ summary(data_pm_interpolated)
 data_pm_standardized <- as.data.frame(scale(data_pm_interpolated))
 
 model <- '
-  # Regressions
-  leaflitter ~ canopy
-  epilithon ~ decapod + canopy  
-  decapod ~ epilithon + leaflitter 
-  macroinvertebrates ~ epilithon + leaflitter + decapod 
+  leaflitter ~ prior("normal(0,1)")*canopy
+  epilithon ~ prior("normal(0,0.5)")*decapod + prior("normal(0,1)")*canopy
+  decapod ~ prior("normal(0,0.5)")*epilithon + prior("normal(0,1)")*leaflitter
+  macroinvertebrates ~ prior("normal(0,1)")*epilithon + prior("normal(0,1)")*leaflitter + prior("normal(0,1)")*decapod
 '
-
-priors <- list(
-  # Normal priors for regression coefficients
-  dp = "normal(0, 1)" # This sets a normal(0, 1) prior on all regression parameters
+mod <- bsem(
+  model,
+  data = data_pm_standardized,
+  control = list(adapt_delta = 0.9999, max_treedepth = 12),
+  n.chains = 4,
+  burnin = 8000,
+  sample = 10000,
+  seed = 14,
+  mcmcfile = TRUE
 )
-
-# Fit the model
-mod <- bcfa(model, data = data_pm_standardized,
-            n.chains = 4, burnin = 8000, sample = 10000,
-            seed = 14, control = list(adapt_delta = 0.9999, 
-            dp = priors$dp, max_treedepth=12), mcmcfile = T)
-
-
-
-pairs(mod)
 summary(mod)
-semPaths(semPlotModel_lavaanModel(model))
+
 
 # extract information
 # https://www.rensvandeschoot.com/tutorials/wambs-blavaan-tutorial/
@@ -83,3 +77,39 @@ for (i in 1:ncol(as.matrix(chain))) {
 
 plot(epilithon~canopy, data=data_pm)
 plot(decapod~epilithon, data=data_pm)
+
+
+
+
+# No used due to warnings -------------------------------------------------
+No used due to warnings:
+Warning messages:
+1: There were 1685 divergent transitions after warmup. See
+https://mc-stan.org/misc/warnings.html#divergent-transitions-after-warmup
+to find out why this is a problem and how to eliminate them. 
+2: Examine the pairs() plot to diagnose sampling problems
+
+
+model <- '
+  # Regressions
+  leaflitter ~ canopy
+  epilithon ~ decapod + canopy  
+  decapod ~ epilithon + leaflitter 
+  macroinvertebrates ~ epilithon + leaflitter + decapod 
+'
+
+priors <- list(
+  # Normal priors for regression coefficients
+  dp = "normal(0, 10)" # This sets a normal(0, 1) prior on all regression parameters
+)
+
+# Fit the model
+mod <- bcfa(model, data = data_pm_standardized,
+            n.chains = 4, burnin = 8000, sample = 10000,
+            seed = 14, control = list(adapt_delta = 0.99995, 
+                                      dp = priors$dp, max_treedepth=10), mcmcfile = T)
+
+
+summary(mod)
+semPaths(semPlotModel_lavaanModel(model))
+
